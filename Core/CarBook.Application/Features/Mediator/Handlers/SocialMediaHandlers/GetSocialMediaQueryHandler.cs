a34017e77f636768d08428c1416466e0b3bf9e27
erenkaraa2 <erenkaraa2@outlook.com>
2024-02-15 08:38:57 +0300

    Add project files.

---
+++ Core/CarBook.Application/Features/Mediator/Handlers/SocialMediaHandlers/GetSocialMediaQueryHandler.cs
@@ -0,0 +1,38 @@
+﻿using CarBook.Application.Features.Mediator.Queries.SocialMediaQueries;
+using CarBook.Application.Features.Mediator.Results.FooterAddressResults;
+using CarBook.Application.Features.Mediator.Results.SocialMediaResults;
+using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.Mediator.Handlers.SocialMediaHandlers
+{
+    public class GetServiceQueryHandler : IRequestHandler<GetSocialMediaQuery, List<GetSocialMediaQueryResult>>
+    {
+        private readonly IRepository<SocialMedia> _repository;
+
+        public GetServiceQueryHandler(IRepository<SocialMedia> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<GetSocialMediaQueryResult>> Handle(GetSocialMediaQuery request, CancellationToken cancellationToken)
+        {
+            var values = await _repository.GetAllAsync();
+            return values.Select(x => new GetSocialMediaQueryResult
+            {
+                Name= x.Name,
+                SocialMediaID=x.SocialMediaID,
+                Icon=x.Icon,
+                Url=x.Url
+            }).ToList();
+        }
+    }
+}
+
+
---
+++ Presentation/CarBook.WebApi/Program.cs
@@ -0,0 +1,83 @@
+using CarBook.Application.Features.CQRS.Handlers.AboutHandlers;
+using CarBook.Application.Features.CQRS.Handlers.BannerHandlers;
+using CarBook.Application.Features.CQRS.Handlers.BrandHandlers;
+using CarBook.Application.Features.CQRS.Handlers.CarHandlers;
+using CarBook.Application.Features.CQRS.Handlers.CategoryHandlers;
+using CarBook.Application.Features.CQRS.Handlers.ContactHandlers;
+using CarBook.Application.Interfaces;
+using CarBook.Application.Interfaces.CarInterfaces;
+using CarBook.Application.Services;
+using CarBook.Persistence.Context;
+using CarBook.Persistence.Repositories;
+using CarBook.Persistence.Repositories.CarRepositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+//Interface tanýmlamasý
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+builder.Services.AddScoped(typeof(ICarRepository), typeof(CarRepository));
+//Context tanýmlamasý
+builder.Services.AddScoped<CarBookContext>();
+//About sýnýf tanýmlamasý
+builder.Services.AddScoped<GetAboutQueryHandler>();
+builder.Services.AddScoped<GetAboutByIdQueryHandler>();
+builder.Services.AddScoped<CreateAboutCommandHandler>();
+builder.Services.AddScoped<UpdateAboutCommandHandler>();
+builder.Services.AddScoped<RemoveAboutCommandHandler>();
+//Banner sýnýf tanýmlamasý
+builder.Services.AddScoped<GetBannerByIdQueryHandler>();
+builder.Services.AddScoped<GetBannerQueryHandler>();
+builder.Services.AddScoped<CreateBannerCommandHandler>();
+builder.Services.AddScoped<UpdateBannerCommandHandler>();
+builder.Services.AddScoped<RemoveBannerCommandHandler>();
+//Brand sýnýf tanýmlanmasý
+builder.Services.AddScoped<GetBrandByIdQueryHandler>();
+builder.Services.AddScoped<GetBrandQueryHandler>();
+builder.Services.AddScoped<CreateBrandCommandHandler>();
+builder.Services.AddScoped<UpdateBrandCommandHandler>();
+builder.Services.AddScoped<RemoveBrandCommandHandler>();
+//Car sýnýf tanýmlanmasý
+builder.Services.AddScoped<GetCarByIdQueryHandler>();
+builder.Services.AddScoped<GetCarQueryHandler>();
+builder.Services.AddScoped<CreateCarCommandHandler>();
+builder.Services.AddScoped<UpdateCarCommandHandler>();
+builder.Services.AddScoped<RemoveCarCommandHandler>();
+builder.Services.AddScoped<GetCarWithBrandQueryHandler>();
+//Categories sýnýf tanýmlanmasý
+builder.Services.AddScoped<GetCategoryByIdQueryHandler>();
+builder.Services.AddScoped<GetCategoryQueryHandler>();
+builder.Services.AddScoped<CreateCategoryCommandHandler>();
+builder.Services.AddScoped<UpdateCategoryCommandHandler>();
+builder.Services.AddScoped<RemoveCategoryCommandHandler>();
+//Contact sýnýf tanýmlanmasý
+builder.Services.AddScoped<GetContactByIdQueryHandler>();
+builder.Services.AddScoped<GetContactQueryHandler>();
+builder.Services.AddScoped<CreateContactCommandHandler>();
+builder.Services.AddScoped<UpdateContactCommandHandler>();
+builder.Services.AddScoped<RemoveContactCommandHandler>();
+//MediatR ServiceRegistation kullanýmý
+builder.Services.AddApplicationService(builder.Configuration);
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
